e54e5f046f8bc5ec603b67dfadfdde7cf424c334
lu <169720213+luizaamorims@users.noreply.github.com>
2025-05-28 22:34:09 -0300

    adicionado numero 0 na lista de numeros e exibição da composição da senha

--- Gerador/Program.cs
+++ Gerador/Program.cs
@@ -6,34 +6,34 @@ bool addMinuscula = false;
 bool addEspecial = false;
 
 
-Console.WriteLine("---------Bem vindo ao Gerador de Senhas---------");
+Console.WriteLine("--------- Bem vindo ao Gerador de Senhas ---------");
 
-Console.WriteLine("Digite o tamanho da senha:");
+Console.Write("Digite o tamanho da senha: (quantos caracteres deseja) ");
 int tamanho = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("Deseja adicionar números? (1 para sim, 2 para não)");
+Console.Write("Deseja adicionar números? (1 para sim, 2 para não) ");
 int numero = int.Parse(Console.ReadLine());
 
 addNumero = numero == 1 ? true : false;
 
-Console.WriteLine("Deseja adicionar letras maiusculas? (1 para sim, 2 para não)");
+Console.Write("Deseja adicionar letras maiusculas? (1 para sim, 2 para não) ");
 int maiuscusla = int.Parse(Console.ReadLine());
 
 addMaiuscula = maiuscusla == 1 ? true : false;
 
-Console.WriteLine("Deseja adicionar letras minusculas? (1 para sim, 2 para não)");
+Console.Write("Deseja adicionar letras minusculas? (1 para sim, 2 para não) ");
 int minuscula = int.Parse(Console.ReadLine());
 
 addMinuscula = minuscula == 1 ? true : false;
 
-Console.WriteLine("Deseja adicionar caracteres especiais? (1 para sim, 2 para não)");
+Console.Write("Deseja adicionar caracteres especiais? (1 para sim, 2 para não) ");
 int especial = int.Parse(Console.ReadLine());
 
 addEspecial = especial == 1 ? true : false;
 
 
-String numeros = "123456789";
+String numeros = "0123456789";
 String letraMaiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 String letraMinuscula = "abcdefghijklmnopqrstuvwxyz";
 String caracteresEspeciais = "!@#$%&*()_+-=[]{}|;:,.<>?";
@@ -70,3 +70,9 @@ for (int i = 0; i < tamanho; i++)
 }
 
 Console.WriteLine("A senha gerada é: " + senha.ToString());
+
+Console.WriteLine("Composição da senha:");
+if (addNumero) Console.WriteLine("  ✓ Números");
+if (addMaiuscula) Console.WriteLine("  ✓ Letras maiúsculas");
+if (addMinuscula) Console.WriteLine("  ✓ Letras minúsculas");
+if (addEspecial) Console.WriteLine("  ✓ Caracteres especiais");
